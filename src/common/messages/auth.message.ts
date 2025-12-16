41b60e11c7b533158f90b9065a07c80a463464bc
phuc.cvh <phuc.cvh@briswell-vn.com>
2025-12-17 00:12:40 +0700

    add messages common

---
+++ src/common/messages/auth.message.ts
@@ -0,0 +1,8 @@
+export const AUTH_MESSAGE = {
+  INVALID_CREDENTIALS: 'Email or password is incorrect',
+  ACCOUNT_DISABLED: 'Account has been disabled',
+  TOKEN_EXPIRED: 'Token expired',
+  REFRESH_TOKEN_REVOKED: 'Refresh token has been revoked',
+  OTP_INVALID: 'OTP is invalid',
+  OTP_EXPIRED: 'OTP has expired',
+};
---
+++ src/common/messages/common.message.ts
@@ -0,0 +1,8 @@
+export const COMMON_MESSAGE = {
+    INVALID: (name: string) => `${name} invalid`,
+    REQUIRED: (name: string) => `${name} is required`,
+    NOT_FOUND: (name: string) => `${name} not found`,
+    ALREADY_EXISTS: (name: string) => `${name} already exists`,
+    FORBIDDEN: 'Forbidden',
+    UNAUTHORIZED: 'Unauthorized',
+};
